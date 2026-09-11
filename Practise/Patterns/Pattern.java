c4827bdcc67eba38d065ca0700f06e73023460ff
Dhiraj <dhirajdh9421@gmail.com>
2026-09-11 16:07:07 +0530

    Added Patterns Practise QUestions

--- Practise/Patterns/Pattern.java
+++ Practise/Patterns/Pattern.java
@@ -2,48 +2,147 @@ package Practise.Patterns;
 
 class Pattern {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         // for (int row = 1; row <= n; row++) {
-        //     // spaces
-        //     for (int col = 1; col <= n - row; col++) {
-        //         System.out.print(" ");
-        //     }
+        // // spaces
+        // for (int col = 1; col <= n - row; col++) {
+        // System.out.print(" ");
+        // }
 
-        //     // stars
-        //     for (int col = 1; col <= n; col++) {
-        //         System.out.print("* ");
-        //     }
-        //     System.out.println();
+        // // stars
+        // for (int col = 1; col <= n; col++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
         // }
 
         // for(int row=1; row<=n; row++){
-        //     for(int col=1; col<=n-row+1; col++){
-        //         System.out.print("* ");
-        //     }
-        //     System.out.println();
+        // for(int col=1; col<=n-row+1; col++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
         // }
 
         // for(int row=1; row<=n; row++){
-        //     //spaces finding 
-        //     for(int col=1; col<=n-row; col++){
-        //         System.out.print(" ");
+        // //spaces finding
+        // for(int col=1; col<=n-row; col++){
+        // System.out.print("  ");
+        // }
+        // //starts
+        // for(int col=1; col<=2*row-1; col++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // for(int row=1; row<=n; row++){
+        //     if(row == 1){
+        //         continue;
         //     }
-        //     //starts
-        //     for(int col=1; col<=2*row-1; col++){
+        // //spaces
+        // for(int col=1; col<=row-1; col++){
+        // System.out.print("  ");
+        // }
+        // //starts
+        // for(int col=1; col<=n*2 - row - (row-1); col++){ // we can write this formula also like this (n*2-row*2+1)
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // for(int row=1; row<=n; row++){
+        // for(int col=1; col<=7; col++){
+        // if(row == 1 || row == n){
+        // System.out.print("* ");
+        // }else{
+        // if(col == 1 || col == 7){
+        // System.out.print("* ");
+        // }else{
+        // System.out.print("  ");
+        // }
+        // }
+        // }System.out.println();
+        // }
+
+        // for (int row = 1; row <= n; row++) {
+
+        //     if (row == 1 || row == 2 || row == n) {
+        //         for (int col = 1; col <= row; col++) {
+        //             System.out.print("* ");
+        //         }
+        //     } else {
         //         System.out.print("* ");
+
+        //         for (int col = 1; col <= (row - 2); col++) {
+        //             System.out.print("  ");
+        //         }
+
+        //         System.out.print("* ");
+
         //     }
         //     System.out.println();
+
+        // }
+
+        // for (int row=1; row<=n; row++){
+        //     for(int col=1; col<=(n-row); col++){
+        //         System.out.print(" ");
+        //     }
+
+        //     if(row == 1 || row == n){
+        //         for(int col=1; col<=(2 * row)-1; col++){
+        //             System.out.print("* ");
+        //         }
+        //     } else{
+        //         System.out.print("* ");
+
+        //         for(int col=1; col<=(2*row)-3; col++){
+        //             System.out.print("  ");
+        //         }
+
+        //         System.out.print("* ");
+        //     }System.out.println();
         // }
 
         for(int row=1; row<=n; row++){
-            //spaces
-            for(int col=1; col<=row-1; col++){
-                System.out.print(" ");
+            for(int col=1; col<=n-row; col++){
+                System.out.print("  ");
             }
-            //starts
-            for(int col=1; col<=n*2 - row - (row-1); col++){ // we can write this formula also like this (n*2-row*2+1)
+
+            if(row == 1){
+                System.out.print("*");
+            }else{
                 System.out.print("* ");
+
+                for(int col=1; col<=row*2-3; col++){
+                    System.out.print("  ");
+                }
+
+                System.out.print("*");
             }
+            System.out.println();
+        }
+
+        for(int row=1; row<=n; row++){
+            if(row == 1){
+                continue;
+            }
+
+            for(int col=1; col<=row-1; col++){
+                System.out.print("  ");
+            }
+
+            if(row == n){
+                System.out.print("*");
+            }else{
+                System.out.print("*");
+
+                for(int col=1; col<=2*(n-row)-1; col++){
+                    System.out.print("  ");
+                }
+
+                System.out.print(" *");
+            } 
             System.out.println();
         }
 
